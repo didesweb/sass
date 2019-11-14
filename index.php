31aea12ca173383551b9d44b8615e29a436e77d3
Roque Arnas <roquearnas@gmail.com>
2019-11-14 21:02:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="app/styles/main.css">
+</head>
+<body>
+	<?php 
+		echo 'Hola';
+	?>
+</body>
+</html>
